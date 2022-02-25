02f0b9ec6b1220afb227e2ab1f26e9072b396710
bigmansteef <90814147+bigmansteef@users.noreply.github.com>
2022-02-24 21:48:48 -0500

    Add Webscraper for the RF rate

--- CAPM PROJ/CAPM MAIN.py
+++ CAPM PROJ/CAPM MAIN.py
@@ -1,6 +1,7 @@
 import bs4 as bs
 import requests
 import codecs
+import lxml
 
 print("Welcome to Steve's CAPM model")
 print("### NOTE ### - Please input percentages as decimals (e.g., 2% = 0.02)")
@@ -27,12 +28,35 @@ def scrapeBeta(ticker):
 
     return beta
 
+def scrapeRF():
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36'}
+    ticker = '%5ETNX'
+    baseURL = 'https://finance.yahoo.com/quote/'
+    mainURL = baseURL + ticker
+
+    # TURN BACK ON WHEN READY FOR PRODUCTION CODE
+    res = requests.get(mainURL, headers)
+    res = res.text
+
+
+    # USE FOR TESTING CODE WITHOUT PINGING WEBSITE
+    # res = codecs.open("pogfile.html", 'r', 'utf-8')
+    # res = res.read()
+
+    soup = bs.BeautifulSoup(res, 'lxml')
+    RF = soup.find_all("fin-streamer", {'data-test':'qsp-price'})
+    RF = RF[0].text
+    RF = float(RF)/100
+
+    return RF
 
 def calcCAPM():
     TCKR = input("What is the ticker of the stock you wish to evaluate?")
-    RF = float(input("What is the current risk free rate"))
-    RM = float(input("What is the current expected market return?"))
+    RF = scrapeRF()
+    RM = 0.11
     BETA = scrapeBeta(TCKR)
+    print(f'the current required return of the market in use is {RM*100}%')
+    print(f'The risk-free rate in use is {round(RF*100, 2)}%')
     print(f'The beta of {TCKR} is = {BETA}')
 
     CAPM = ((RM - RF) * BETA) + RF
